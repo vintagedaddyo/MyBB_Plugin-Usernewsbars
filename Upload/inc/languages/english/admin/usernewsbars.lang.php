02f447a8932cffa2a8665dda2a14ac8d75ca7a64
Vintagedaddyo <vintagedaddyo@gmail.com>
2018-12-24 12:13:04 -0800

    Minor Commit

---
+++ Upload/inc/languages/english/admin/usernewsbars.lang.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * MyBB: User Newsbars
+ *
+ * File: usernewsbars.lang.php
+ * 
+ * Authors: Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.0
+ * 
+ */
+// plugin_info
+
+$l['usernewsbars_name'] = 'User Newsbars';
+$l['usernewsbars_desc'] = 'Adds user newsbars to the index and portal page of your forum.';
+$l['usernewsbars_web'] = 'http://community.mybb.com/user-6029.html';
+$l['usernewsbars_auth'] = 'Vintagedaddyo';
+$l['usernewsbars_authsite'] = 'http://community.mybb.com/user-6029.html';
+$l['usernewsbars_ver'] = '1.2';
+$l['usernewsbars_compat'] = '18*';
+
+// Setting Group
+
+$l['usernewsbars_title_setting_group'] = 'User Newsbars';
+$l['usernewsbars_description_setting_group'] = 'Settings for the User Newsbars plugin.';
+
+// Setting 1
+
+$l['usernewsbars_title_setting_1'] = 'Do you want to enable User Newsbars Plugin Globally?';
+$l['usernewsbars_description_setting_1'] = 'If you set this option to yes, this plugin be active globally.';
+
+// Setting 2
+
+$l['usernewsbars_title_setting_2'] = 'Alert Display';
+$l['usernewsbars_description_setting_2'] = 'Enter the text you want to display in Alert.';
+
+// Setting 3
+
+$l['usernewsbars_title_setting_3'] = 'CSS for User Alert';
+$l['usernewsbars_description_setting_3'] = 'This controls the styling for user alert.';
+
+// Setting 4
+
+$l['usernewsbars_title_setting_4'] = 'Do you want to enable User Newsbars Alert?';
+$l['usernewsbars_description_setting_4'] = 'If you set this option to yes, Alert will be active on your board.';
+
+// Setting 5
+
+$l['usernewsbars_title_setting_5'] = 'Notice 1 Display';
+$l['usernewsbars_description_setting_5'] = 'Enter the text you want to display in Notice 1.';
+
+// Setting 6
+
+$l['usernewsbars_title_setting_6'] = 'CSS for User Notice 1';
+$l['usernewsbars_description_setting_6'] = 'This controls the styling for user notice1.';
+
+// Setting 7
+
+$l['usernewsbars_title_setting_7'] = 'Do you want to enable User Newsbars Notice 1?';
+$l['usernewsbars_description_setting_7'] = 'If you set this option to yes, Notice 1 will be active on your board.';
+
+// Setting 8
+
+$l['usernewsbars_title_setting_8'] = 'Notice 2 Display';
+$l['usernewsbars_description_setting_8'] = 'Enter the text you want to display in User Notice 2.';
+
+// Setting 9
+
+$l['usernewsbars_title_setting_9'] = 'CSS for User Notice 2';
+$l['usernewsbars_description_setting_9'] = 'This controls the styling for user notice2.';
+
+// Setting 10
+
+$l['usernewsbars_title_setting_10'] = 'Do you want to enable User Newsbars Notice 2?';
+$l['usernewsbars_description_setting_10'] = 'If you set this option to yes, Notice 2 will be active on your board.';
+
+?> 
--- Upload/inc/plugins/usernewsbars.php
+++ Upload/inc/plugins/usernewsbars.php
@@ -20,27 +20,37 @@ $plugins->add_hook('global_start', 'usernewsbars_global_start');
 
 function usernewsbars_info()
 {
-    return array(
-        "name" => "User Newsbars",
-        "description" => "Adds user specific newsbars to the header_welcomeblock_member template of your forum",
-        "website" => "http://community.mybb.com/user-6029.html",
-        "author" => "Vintagedaddyo",
-        "authorsite" => "http://community.mybb.com/user-6029.html",
-        "version" => "1.0",
-        "guid" => "",
-        "compatability" => "18*"
+    global $lang;
+
+    // Load Language
+
+    $lang->load("usernewsbars");
+
+    $lang->usernewsbars_desc = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="float:right;">' . '<input type="hidden" name="cmd" value="_s-xclick">' . '<input type="hidden" name="hosted_button_id" value="AZE6ZNZPBPVUL">' . '<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">' . '<img alt="" border="0" src="https://www.paypalobjects.com/pl_PL/i/scr/pixel.gif" width="1" height="1">' . '</form>' . $lang->usernewsbars_desc;
+    return Array(
+        'name' => $lang->usernewsbars_name,
+        'description' => $lang->usernewsbars_desc,
+        'website' => $lang->usernewsbars_web,
+        'author' => $lang->usernewsbars_auth,
+        'authorsite' => $lang->usernewsbars_authsite,
+        'version' => $lang->usernewsbars_ver,
+        'compatibility' => $lang->usernewsbars_compat
     );
 }
 
 function usernewsbars_activate()
 {
     global $settings, $mybb, $db, $lang;
-    
+
+    // Load Language
+
+    $lang->load("usernewsbars");
+
     $usernewsbars_group = array(
         'gid' => '0',
         'name' => 'usernewsbars',
-        'title' => 'User Newsbars',
-        'description' => 'Settings for the User Newsbars plugin',
+        'title' => $lang->usernewsbars_title_setting_group,
+        'description' => $lang->usernewsbars_description_setting_group,
         'disporder' => "1",
         'isdefault' => "0"
     );
@@ -52,8 +62,8 @@ function usernewsbars_activate()
     $usernewsbars_setting_1 = array(
         'sid' => '0',
         'name' => 'usernewsbars_enable_global',
-        'title' => 'Do you want to enable User Newsbars Plugin Globally?',
-        'description' => 'If you set this option to yes, this plugin be active globally.',
+        'title' => $lang->usernewsbars_title_setting_1,
+        'description' => $lang->usernewsbars_description_setting_1,
         'optionscode' => 'yesno',
         'value' => '1',
         'disporder' => '1',
@@ -63,8 +73,8 @@ function usernewsbars_activate()
     $usernewsbars_setting_2 = array(
         'sid' => '0',
         'name' => 'usernewsbars_input_1',
-        'title' => 'Alert Display',
-        'description' => 'Enter the text you want to display in Alert.',
+        'title' => $lang->usernewsbars_title_setting_2,
+        'description' => $lang->usernewsbars_description_setting_2,
         'optionscode' => 'textarea',
         'value' => '<strong>Latest MyBB Release:</strong> <a href="http://blog.mybb.com/2018/09/11/mybb-1-8-19-released-security-maintenance-release/">MyBB 1.8.19 Released — Security & Maintenance Release</a> <span class="date">(September 11, 2018)</span>',
         'disporder' => '2',
@@ -74,8 +84,8 @@ function usernewsbars_activate()
     $usernewsbars_setting_3 = array(
         'sid' => '0',
         'name' => 'usernewsbars_css_input_1',
-        'title' => 'CSS for User Alert',
-        'description' => 'This controls the styling for user alert.',
+        'title' => $lang->usernewsbars_title_setting_3,
+        'description' => $lang->usernewsbars_description_setting_3,
         'optionscode' => 'textarea',
         'value' => '
 .user-alert {
@@ -111,8 +121,8 @@ function usernewsbars_activate()
     $usernewsbars_setting_4 = array(
         'sid' => '0',
         'name' => 'usernewsbars_enable_input_1',
-        'title' => 'Do you want to enable User Newsbars Alert?',
-        'description' => 'If you set this option to yes, Alert will be active on your board.',
+        'title' => $lang->usernewsbars_title_setting_4,
+        'description' => $lang->usernewsbars_description_setting_4,
         'optionscode' => 'yesno',
         'value' => '1',
         'disporder' => '4',
@@ -122,8 +132,8 @@ function usernewsbars_activate()
     $usernewsbars_setting_5 = array(
         'sid' => '0',
         'name' => 'usernewsbars_input_2',
-        'title' => 'Notice 1 Display',
-        'description' => 'Enter the text you want to display in Notice 1.',
+        'title' => $lang->usernewsbars_title_setting_5,
+        'description' => $lang->usernewsbars_description_setting_5,
         'optionscode' => 'textarea',
         'value' => '<strong>Latest news from the MyBB Blog:</strong> <a href="http://blog.mybb.com/2018/08/29/blueprinting-automatic-updates-for-php-applications/">Blueprinting Automatic Updates for PHP Applications</a> <span class="date">(August 29, 2018</span>',
         'disporder' => '5',
@@ -133,8 +143,8 @@ function usernewsbars_activate()
     $usernewsbars_setting_6 = array(
         'sid' => '0',
         'name' => 'usernewsbars_css_input_2',
-        'title' => 'CSS for User Notice 1',
-        'description' => 'This controls the styling for user notice1.',
+        'title' => $lang->usernewsbars_title_setting_6,
+        'description' => $lang->usernewsbars_description_setting_6,
         'optionscode' => 'textarea',
         'value' => '
 .user-notice1 {
@@ -170,8 +180,8 @@ function usernewsbars_activate()
     $usernewsbars_setting_7 = array(
         'sid' => '0',
         'name' => 'usernewsbars_enable_input_2',
-        'title' => 'Do you want to enable User Newsbars Notice 1?',
-        'description' => 'If you set this option to yes, Notice 1 will be active on your board.',
+        'title' => $lang->usernewsbars_title_setting_7,
+        'description' => $lang->usernewsbars_description_setting_7,
         'optionscode' => 'yesno',
         'value' => '1',
         'disporder' => '7',
@@ -181,8 +191,8 @@ function usernewsbars_activate()
     $usernewsbars_setting_8 = array(
         'sid' => '0',
         'name' => 'usernewsbars_input_3',
-        'title' => 'Notice 2 Display',
-        'description' => 'Enter the text you want to display in User Notice 2.',
+        'title' => $lang->usernewsbars_title_setting_8,
+        'description' => $lang->usernewsbars_description_setting_8,
         'optionscode' => 'textarea',
         'value' => '<strong>Are you on the </strong><a href="http://community.mybb.com/member.php?action=register">MyBB Community Forums</a><strong>&nbsp;?</strong> - Sign up for notification of new MyBB releases and updates.',
         'disporder' => '8',
@@ -192,8 +202,8 @@ function usernewsbars_activate()
     $usernewsbars_setting_9 = array(
         'sid' => '0',
         'name' => 'usernewsbars_css_input_3',
-        'title' => 'CSS for User Notice 2',
-        'description' => 'This controls the styling for user notice2.',
+        'title' => $lang->usernewsbars_title_setting_9,
+        'description' => $lang->usernewsbars_description_setting_9,
         'optionscode' => 'textarea',
         'value' => '
 .user-notice2 {
@@ -230,8 +240,8 @@ function usernewsbars_activate()
     $usernewsbars_setting_10 = array(
         'sid' => '0',
         'name' => 'usernewsbars_enable_input_3',
-        'title' => 'Do you want to enable User Newsbars Notice 2?',
-        'description' => 'If you set this option to yes, Notice 2 will be active on your board.',
+        'title' => $lang->usernewsbars_title_setting_10,
+        'description' => $lang->usernewsbars_description_setting_10,
         'optionscode' => 'yesno',
         'value' => '1',
         'disporder' => '10',
@@ -285,7 +295,7 @@ function usernewsbars_activate()
     
     // activate on global
     
-    find_replace_templatesets("header_welcomeblock_member", "#" . preg_quote("<br class=\"clear\" />") . "#i", "<br class=\"clear\" />\r\n</div><div class=\"wrapper\" style=\"width:84%;\">
+    find_replace_templatesets("header_welcomeblock_member", "#" . preg_quote("<br class=\"clear\" />") . "#i", "<br class=\"clear\" />\r\n</div><div class=\"wrapper\">
         {\$usernewsbars_1}
         {\$usernewsbars_2}
         {\$usernewsbars_3}");
@@ -321,7 +331,7 @@ function usernewsbars_deactivate()
     
     // deactivate on global
     
-    find_replace_templatesets("header_welcomeblock_member", "#" . preg_quote("\r\n</div><div class=\"wrapper\" style=\"width:84%;\">
+    find_replace_templatesets("header_welcomeblock_member", "#" . preg_quote("\r\n</div><div class=\"wrapper\">
         {\$usernewsbars_1}
         {\$usernewsbars_2}
         {\$usernewsbars_3}") . "#i", "", 0);

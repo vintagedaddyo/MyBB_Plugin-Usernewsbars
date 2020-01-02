dfc944235781d9e51ef008532c01eb9e831dc74f
Vintagedaddyo <vintagedaddyo@gmail.com>
2020-01-01 22:45:25 -0800

    user requested german localization
    not the best translation

---
+++ Upload/inc/languages/deutsch_du/admin/usernewsbars.lang.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * MyBB: Benutzer-Newsbars
+ *
+ * Datei: usernewsbars.lang.php
+ *
+ * Autoren: Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin-Version: 1.0
+ *
+ */
+
+// Plugin-Informationen
+
+$l['usernewsbars_name'] = 'Benutzer-Newsbars';
+$l['usernewsbars_desc'] = 'Fügt der header_welcomeblock_member-Vorlage Ihres Forums Benutzer-Newsbars hinzu, um die Sichtbarkeit zu erhöhen.';
+$l['usernewsbars_web'] = 'http://community.mybb.com/user-6029.html';
+$l['usernewsbars_auth'] = 'Vintagedaddyo';
+$l['usernewsbars_authsite'] = 'http://community.mybb.com/user-6029.html';
+$l['usernewsbars_ver'] = '1.0';
+$l['usernewsbars_compat'] = '18*';
+
+// Gruppe einstellen
+
+$l['usernewsbars_title_setting_group'] = 'Benutzer-Newsbars';
+$l['usernewsbars_description_setting_group'] = 'Einstellungen für das User Newsbars Plugin.';
+
+// Einstellung 1
+
+$l['usernewsbars_title_setting_1'] = 'Möchten Sie das Benutzer-Newsbar-Plugin global aktivieren?';
+$l['usernewsbars_description_setting_1'] = 'Wenn Sie diese Option auf yes setzen, ist dieses Plugin global aktiv.';
+
+// Einstellung 2
+
+$l['usernewsbars_title_setting_2'] = 'Alarmanzeige';
+$l['usernewsbars_description_setting_2'] = 'Geben Sie den Text ein, den Sie in Alert anzeigen möchten.';
+
+// Einstellung 3
+
+$l['usernewsbars_title_setting_3'] = 'CSS für Benutzerbenachrichtigung';
+$l['usernewsbars_description_setting_3'] = 'Dies steuert das Styling für die Benutzerbenachrichtigung.';
+
+// Einstellung 4
+
+$l['usernewsbars_title_setting_4'] = 'Möchten Sie den Benutzer-Newsbar-Alarm aktivieren?';
+$l['usernewsbars_description_setting_4'] = 'Wenn Sie diese Option auf yes setzen, ist die Benachrichtigung auf Ihrem Board aktiv.';
+
+// Einstellung 5
+
+$l['usernewsbars_title_setting_5'] = 'Beachten 1 Display';
+$l['usernewsbars_description_setting_5'] = 'Geben Sie den Text ein, den Sie in Beachten 1 anzeigen möchten.';
+
+// Einstellung 6
+
+$l['usernewsbars_title_setting_6'] = 'CSS für Benutzerhinweis 1';
+$l['usernewsbars_description_setting_6'] = 'Dies steuert das Styling für den Benutzer Beachten1.';
+
+// Einstellung 7
+
+$l['usernewsbars_title_setting_7'] = 'Möchten Sie Benutzer-Newsbars Beachten 1 aktivieren?';
+$l['usernewsbars_description_setting_7'] = 'Wenn Sie diese Option auf yes setzen, ist Beachten 1 auf Ihrem Board aktiv.';
+
+// Einstellung 8
+
+$l['usernewsbars_title_setting_8'] = 'Hinweis 2-Anzeige';
+$l['usernewsbars_description_setting_8'] = 'Geben Sie den Text ein, den Sie in Benutzerhinweis 2 anzeigen möchten.';
+
+// Einstellung 9
+
+$l['usernewsbars_title_setting_9'] = 'CSS für Benutzerhinweis 2';
+$l['usernewsbars_description_setting_9'] = 'Dies steuert das Styling für den Benutzer Beachten2.';
+
+// Einstellung 10
+
+$l['usernewsbars_title_setting_10'] = 'Möchten Sie Benutzer-Newsbars Beachten 2 aktivieren?';
+$l['usernewsbars_description_setting_10'] = 'Wenn Sie diese Option auf yes setzen, ist Beachten 2 auf Ihrem Board aktiv.';
+
+?> 
